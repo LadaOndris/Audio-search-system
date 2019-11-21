e5e0ba72383d19a876a8c10d2979df31557d152a
Ladislav Ondris <ladislav.ondris@seznam.cz>
2019-11-21 15:17:50 +0100

    Almost finished

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ ISS project
 import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sf
+from pydub import AudioSegment
 import IPython
 from scipy.signal import spectrogram, lfilter, freqz, tf2zpk
 import scipy
@@ -82,21 +83,33 @@ def compute_correlations(F, Q):
     total_steps = F_length - Q_length
     
     correlations = np.zeros(F_length)
+    
     for i in range(total_steps):
         corr = compute_correlation(Q, F, i)
         correlations[i] = corr
+        
     return correlations
 
-def draw_correlations(filename, Q1, Q2): 
-    data_s, fs_s = sf.read(filename)
+def find_peeks(correlations, t, threshold = 0.6):
     
-    F_frames_count, F_t, F = get_features(data_s, fs_s)
+    peeks = []  
+    is_ascending = False
     
-    correlations_q1 = compute_correlations(F, Q1)
-    correlations_q2 = compute_correlations(F, Q2)
-        
+    for index, corr in enumerate(correlations[1:]):
+        if threshold < corr:
+            previous_corr = correlations[index]
+            if previous_corr < corr:
+                is_ascending = True
+            else:
+                if is_ascending:
+                    peeks.append((previous_corr, t[index]))
+                is_ascending = False
+    return peeks
+            
+
+def draw_main_results(filename, data_s, F_t, F_frames_count, F, corrs_q1, corrs_q2):
     fig, axs = plt.subplots(3, 1, figsize=(10,5))
-    axs[0].set_title('"essentially" and "exercise" vs %s' % os.path.split(filename)[-1])
+    axs[0].set_title('"essentially" and "exercise" vs %s' % filename)
     axs[0].plot(np.arange(data_s.size) / Fs, data_s)
     axs[0].set_ylabel('signal')
     axs[0].margins(x=0)
@@ -104,16 +117,17 @@ def draw_correlations(filename, Q1, Q2):
     axs[1].pcolormesh(F_t, range(F_frames_count), F)
     axs[1].set_ylabel('features')
     
-    axs[2].plot(F_t, correlations_q1)
-    axs[2].plot(F_t, correlations_q2)
+    axs[2].plot(F_t, corrs_q1)
+    axs[2].plot(F_t, corrs_q2)
     axs[2].set_ylabel('scores')
     axs[2].set_xlabel('Time [s]')
     axs[2].legend(['essentially', 'exercise'])
     axs[2].margins(x=0)
     axs[2].set_ylim([0, 1])
     
-    #plt.savefig('../docs/%s.png' % Path(filename).resolve().stem)
+    plt.savefig('../docs/%s.png' % Path(filename).resolve().stem)
     
+
 def task3_spectogram():
     filepath = '../sentences/sx186.wav'
     filename = Path(filepath).resolve().stem
@@ -121,7 +135,27 @@ def task3_spectogram():
     f, t, sgr = spectrogram(data_s, Fs, nperseg=N, noverlap=woverlap)
     show_spectogram(f, t, sgr, filename)
     
-#task3_spectogram()
+def cut_audio(filename, cut_from, cut_to, cut_filename):
+    cut_from *= 1000
+    cut_to *= 1000
+    audio = AudioSegment.from_wav(filename)
+    new_audio = audio[cut_from:cut_to]
+    new_audio.export(cut_filename, format="wav") 
+
+def analyse_file(filepath, Q1, Q2, draw): 
+    data_s, fs_s = sf.read(filepath)    
+    F_frames_count, F_t, F = get_features(data_s, fs_s)
+    
+    corrs_q1 = compute_correlations(F, Q1)
+    corrs_q2 = compute_correlations(F, Q2)
+    
+    if draw:
+        filename = os.path.split(filepath)[-1]
+        draw_main_results(filename, data_s, F_t, F_frames_count, F, corrs_q1, corrs_q2)
+    
+    return corrs_q1, corrs_q2, F_t
+
+    
 
 filenames = [f for f in glob.glob('../sentences/*.wav')]
 
@@ -136,10 +170,30 @@ data_s, fs_s = sf.read('../sentences/sa2.wav')
 f, t, sgr = spectrogram(data_s, Fs, nperseg=N, noverlap=woverlap, nfft=511)
 print(sgr.shape)
 """
-for filename in filenames:
-    draw_correlations(filename, Q, Q2)
+for filepath in filenames:
+    filename = os.path.split(filepath)[-1]
+    corrs_q1, corrs_q2, F_t = analyse_file(filepath, Q, Q2, True)
     
-
+    # find peeks and cut audio
+    
+    peeks = find_peeks(corrs_q1, F_t,  0.7)
+    print('%s - q1: ' % filepath, peeks)
+    for corr, time in peeks:
+        cut_filename = '../hits/q1_%s' % filename
+        sample_from = time * Fs
+        sample_to = (time + Q_t[-1]) * Fs
+        print('Cutting from sample %d to %d.' % (sample_from, sample_to))
+        #cut_audio(filepath, time, time + Q_t[-1], cut_filename) # cut from 'time' to 'time + Q_t[-1]'
+        
+    peeks = find_peeks(corrs_q2, F_t, 0.6)
+    print('%s - q2: ' % filepath, peeks)
+    for corr, time in peeks:
+        cut_filename = '../hits/q2_%s' % filename
+        sample_from = time * Fs
+        sample_to = (time + Q_t[-1]) * Fs
+        print('Cutting from sample %d to %d.' %  (sample_from, sample_to))
+        #cut_audio(filepath, time, time + Q2_t[-1], cut_filename) # cut from 'time' to 'time + Q2_t[-1]'
+    
 
 
 
